e383c0d5acab8e85d9672fff029e9aca72ccb9ca
Kelly Fam <kellyfam@dhcp-hol-3393.eduroam.cornell.edu>
2017-11-08 20:59:13 -0500

    changes to base and member page

--- members.php
+++ members.php
@@ -8,6 +8,8 @@
   <link rel="stylesheet" type="text/css" href="styles/members.css" media="all"/>
   <script type="text/javascript" src="scripts/jquery-3.2.1.min.js"></script>
   <script type="text/javascript" src="scripts/showMenu.js"></script>
+  <script type="text/javascript" src="scripts/showModal.js"></script>
+
 </head>
 
 <body>
@@ -21,83 +23,189 @@
     <!-- Main Content Implementation Starts Here -->
     <div class="main">
       <!-- this is the menu pic used for mobile view, pls leave alone  -->
+      <?php include "includes/menubar.php"; ?>
 
-      <div class="container">
+      <!-- this is the banner/slideshow for the landing page before scrolling -->
+      <div class="banner">
+        <div class="banner-text">
+          <h1>Members</h1>
+          <!-- <p>And I'm a Photographer</p> -->
+          <!-- <a id="button" href="#container">Get to Know Them</a> -->
+        </div>
+      </div>
 
-        <?php include "includes/menubar.php"; ?>
+      <div class="container" id="container">
+
+
 
         <!-- row 1 -->
         <div class="row">
-          <div class="col-1">
+          <div class="col-1" >
+            <div class="box-text">
+              <button id="myBtn"><h2>Name</h2></button>
+              <p>shadows nickname</p>
+            </div>
           </div>
           <div class="col-2">
+            <div class="box-text">
+              <button id="myBtn"><h2>Name</h2></button>
+              <p>shadows nickname</p>
+            </div>
           </div>
           <div class="col-3">
+            <div class="box-text">
+              <button id="myBtn"><h2>Name</h2></button>
+              <p>shadows nickname</p>
+            </div>
           </div>
         </div>
+
+
 
         <!-- row 2 -->
         <div class="row">
           <div class="col-1">
+            <div class="box-text">
+              <button id="myBtn"><h2>Name</h2></button>
+              <p>shadows nickname</p>
+            </div>
           </div>
           <div class="col-2">
+            <div class="box-text">
+              <button id="myBtn"><h2>Name</h2></button>
+              <p>shadows nickname</p>
+            </div>
           </div>
           <div class="col-3">
+            <div class="box-text">
+              <button id="myBtn"><h2>Name</h2></button>
+              <p>shadows nickname</p>
+            </div>
           </div>
         </div>
 
         <!-- row 3 -->
         <div class="row">
           <div class="col-1">
+            <div class="box-text">
+              <button id="myBtn"><h2>Name</h2></button>
+              <p>shadows nickname</p>
+            </div>
           </div>
           <div class="col-2">
+            <div class="box-text">
+              <button id="myBtn"><h2>Name</h2></button>
+              <p>shadows nickname</p>
+            </div>
           </div>
           <div class="col-3">
+            <div class="box-text">
+              <button id="myBtn"><h2>Name</h2></button>
+              <p>shadows nickname</p>
+            </div>
           </div>
         </div>
 
         <!-- row 4-->
         <div class="row">
           <div class="col-1">
+            <div class="box-text">
+              <button id="myBtn"><h2>Name</h2></button>
+              <p>shadows nickname</p>
+            </div>
           </div>
           <div class="col-2">
+            <div class="box-text">
+              <button id="myBtn"><h2>Name</h2></button>
+              <p>shadows nickname</p>
+            </div>
           </div>
           <div class="col-3">
+            <div class="box-text">
+              <button id="myBtn"><h2>Name</h2></button>
+              <p>shadows nickname</p>
+            </div>
           </div>
         </div>
 
         <!-- row 5 -->
         <div class="row">
           <div class="col-1">
+            <div class="box-text">
+              <button id="myBtn"><h2>Name</h2></button>
+              <p>shadows nickname</p>
+            </div>
           </div>
           <div class="col-2">
+            <div class="box-text">
+              <button id="myBtn"><h2>Name</h2></button>
+              <p>shadows nickname</p>
+            </div>
           </div>
           <div class="col-3">
+            <div class="box-text">
+              <button id="myBtn"><h2>Name</h2></button>
+              <p>shadows nickname</p>
+            </div>
           </div>
         </div>
 
         <!-- row 6 -->
         <div class="row">
           <div class="col-1">
+            <div class="box-text">
+              <button id="myBtn"><h2>Name</h2></button>
+              <p>shadows nickname</p>
+            </div>
           </div>
           <div class="col-2">
+            <div class="box-text">
+              <button id="myBtn"><h2>Name</h2></button>
+              <p>shadows nickname</p>
+            </div>
           </div>
           <div class="col-3">
+            <div class="box-text">
+              <button id="myBtn"><h2>Name</h2></button>
+              <p>shadows nickname</p>
+            </div>
           </div>
         </div>
 
         <!-- row 7 -->
         <div class="row">
           <div class="col-1">
+            <div class="box-text">
+              <button id="myBtn"><h2>Name</h2></button>
+              <p>shadows nickname</p>
+            </div>
           </div>
           <div class="col-2">
+            <div class="box-text">
+              <button id="myBtn"><h2>Name</h2></button>
+              <p>shadows nickname</p>
+            </div>
           </div>
           <div class="col-3">
+            <div class="box-text">
+              <button id="myBtn"><h2>Name</h2></button>
+              <p>shadows nickname</p>
+            </div>
           </div>
         </div>
 
 
       </div>
+
+    <div id="myModal" class="modal">
+
+      <!-- Modal content -->
+      <div class="modal-content">
+        <span class="close">&times;</span>
+        <p>Some text in the Modal..</p>
+      </div>
+
+    </div>
 
     </div>
 
