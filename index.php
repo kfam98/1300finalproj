41b70cb2c1e86f82c7ba6c0ecaa2bd43e4245cc7
Cedric Castillo <cedric@dhcp-hol-2483.eduroam.cornell.edu>
2017-11-09 21:05:58 -0500

    you have to pu quotes

--- index.php
+++ index.php
@@ -30,6 +30,27 @@
 
     </div>
 
+    <div class="indexInfo">
+      <h1> About </h1>
+
+      <h2> Mission </h2>
+      <p>
+        Shadows Dance Troupe exists to provide Cornell students with the opportunity to dance and perform, with qualifications including experience, merit, enthusiasm, effort, and personal commitment. Shadows holds auditions twice a year: at the beginning of the fall semester and at the beginning of the spring semester. We are always looking for strong, talented dancers. If you are interested in auditioning, please look out for information on this page or contact one of our co-chairs!
+      </p>
+
+      <h2> About </h2>
+      <p>
+        Shadows Dance Troupe was founded in 1993 at Cornell University. A group of energetic and versatile dancers, Troupe members come from all different dance backgrounds. Shadows routines are innovative and represent the different strengths of group members. Whether it be jazz, tap, lyrical, modern or hip hop, every Shadow absolutely loves to practice and perform. During two shows a year, Fall Step and Spring Heat, as well as during a variety of guest performances throughout the year, Shadow's wows its audience!
+      </p>
+
+      <h2> Meetings </h2>
+      <p>
+        We meet three times a week in WSH and Helen Newman, depending on availability of rehearsal space.
+      </p>
+
+
+    </div>
+
   </div>
 </body>
 </html>
