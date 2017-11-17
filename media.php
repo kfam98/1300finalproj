a4f7f3af3fb1385199ff89796d845c7e2c99fd3e
maw367 <maw367@cornell.edu>
2017-11-16 21:24:07 -0500

    fixed slideshow

--- media.php
+++ media.php
@@ -6,7 +6,6 @@
   <title>Shadows Dance Troupe</title>
   <link rel="stylesheet" type="text/css" href="styles/base.css" media="all"/>
   <link rel="stylesheet" type="text/css" href="styles/media.css" media="all"/>
-  <script type="text/javascript" src="scripts/slideshow.js"></script>
   <script type="text/javascript" src="scripts/jquery-3.2.1.min.js"></script>
   <script type="text/javascript" src="scripts/showMenu.js"></script>
   <script type="text/javascript" src="scripts/scroll.js"></script>
@@ -26,36 +25,33 @@
       <h1 class="central">MEDIA</h1>
     </div>
     <img id="scroll" alt="scroll" src="images/icons/arrow.png">
-    <div class="Media-box">
-      <div class="slideshow-container">
-        <div class="mySlides fade">
-          <div class="numbertext">1 / 3</div>
-          <img src="images/Group1.jpg" alt="Group1" style="width:100%">
-          <div class="text">Caption Text</div>
-        </div>
+    <div class="slideshow-container">
+<div class="Media-box">
+<div class="mySlides fade">
+  <div class="numbertext">1 / 3</div>
+  <img src="images/Group1.jpg" alt="Group1" style="width:100%">
+</div>
 
-        <div class="mySlides fade">
-          <div class="numbertext">2 / 3</div>
-          <img src="images/Group2.jpg" alt="Group2" style="width:100%">
-          <div class="text">Caption Two</div>
-        </div>
+<div class="mySlides fade">
+  <div class="numbertext">2 / 3</div>
+  <img src="images/Group2.jpg" alt="Group2" style="width:100%">
+</div>
 
-        <div class="mySlides fade">
-          <div class="numbertext">3 / 3</div>
-          <img src="images/Group3.jpg" alt="Group3" style="width:100%">
-          <div class="text">Caption Three</div>
-        </div>
+<div class="mySlides fade">
+  <div class="numbertext">3 / 3</div>
+  <img src="images/Group3.jpg" alt="Group3" style="width:100%">
+</div>
 
-        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-        <a class="next" onclick="plusSlides(1)">&#10095;</a>
-      </div>
+</div>
+<br>
 
-      <div style="text-align:center">
-        <span class="dot" onclick="currentSlide(1)"></span>
-        <span class="dot" onclick="currentSlide(2)"></span>
-        <span class="dot" onclick="currentSlide(3)"></span>
-      </div>
-    </div>
+<div style="text-align:center">
+  <span class="dot"></span>
+  <span class="dot"></span>
+  <span class="dot"></span>
+</div>
+<script type="text/javascript" src="scripts/slideshow_auto.js"></script>
+</div>
 
 
   </div>
