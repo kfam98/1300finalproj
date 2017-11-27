60497cbba361c48eeb71de4bbe30e1926d5d806b
maw367 <maw367@cornell.edu>
2017-11-26 21:41:46 -0500

    slideshow

--- scripts/dot.js
+++ scripts/dot.js
@@ -1,15 +1,16 @@
-$(document).ready(function()) {
-  $('.dot').bind('click', function(){
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+$(document).ready(function() {
+  var i;
+  var slideIndex = 1
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+
+  $('.dot').click(function(n) {
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    slides.className.replace(" active", "")
+    if (slideIndex == i) {
+    slides.className.replace("", " active")
+  }
   });
-}
+
+});
