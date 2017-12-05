27e2c437d87955312e510c72317f9928dc1e2777
Kelly Fam <kellyfam@dhcp-hol-2310.eduroam.cornell.edu>
2017-12-04 21:25:45 -0500

    lol

--- index.php
+++ index.php
@@ -14,7 +14,6 @@
   <!-- javascript -->
   <script type="text/javascript" src="scripts/jquery-3.2.1.min.js"></script>
   <script type="text/javascript" src="scripts/showMenu.js"></script>
-  <script type="text/javascript" src="scripts/emailForm.js"></script>
   <script type="text/javascript" src="scripts/scroll.js"></script>
 
 </head>
@@ -25,9 +24,6 @@
 
     <!-- Sidebar Implemented Starts Here -->
     <?php include "includes/sidebar.php"; ?>
-
-
-
 
     <!-- Main Content Implementation Starts Here -->
     <div class="main">
