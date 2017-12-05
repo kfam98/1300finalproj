b1b9f9318561825ab51a32a1adc6350fb8538cf5
maw367 <maw367@cornell.edu>
2017-12-04 22:03:09 -0500

    form

--- scripts/form.js
+++ scripts/form.js
@@ -1,14 +1,19 @@
 $(document).ready(function() {
-
   // Validate form on submit
   $("#emailForm").on("submit", function() {
     var emailIsValid = $("#sidebarEmail").prop("validity").valid;
-    var emailIsEmpty = if $("#sidebarEmail"); 
-    if ((emailIsValid) && emailIsEmpty) {
+    if ($("#sidebarEmail")=="") {
+      var emailIsEmpty = true;
+    } else {
+      var emailIsEmpty = false;
+    }
+    if ((emailIsValid) && (!emailIsEmpty)) {
       $("#sidebarEmailError").hide();
     } else {
       $("#sidebarEmailError").show();
       return false;
     }
   });
+console.log(emailIsValid);
+console.log(emailIsEmpty);
 });
